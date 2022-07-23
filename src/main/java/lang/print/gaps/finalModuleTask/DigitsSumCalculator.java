01889e6d1227290b9c9b4af68b89fb9891ee4a51
Yoqub <yyboboraximov@gmail.com>
2022-07-23 10:17:19 +0500

    lastTask

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int one = number % 10;
+        int two = number % 100 / 10;
+        int three = number % 1000 / 100;
+        int four = number % 10000 / 1000;
+
+        int result = one + two + three + four;
+        System.out.println(result);
     }
+
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,6 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-
+        boolean result = (number >0) ? true : false;
+        System.out.println(result);
     }
 }
